7c6354b1b2fcce07ba5159cfe5bd1f61163adc60
Georgia Tardy <georgiatardyart@gmail.com>
2017-08-19 20:16:55 -0400

    made additions to database

--- models/artwork.js
+++ models/artwork.js
@@ -1,24 +1,18 @@
 const mongoose = require('mongoose');
 
-// get a reference to Schema
+
 const Schema = mongoose.Schema;
 
 // ==== Create a schema for art collection ==== //
 const artSchema = new Schema({
   title: { type: String, required: true },
   medium: { type: String },
-  genre: { type: String },
-  yearMade: { type: Number, required: true },
-  // custom: { type: Boolean, required: true },
-  // materials: [],
-  description: {
-    colorScheme: { type: String },
-    content: { type: String },
-    // shape: { type: String },
-  }
+  price: { type: Number },
+  style: { type: String},
+
 });
 
-// create a model for a Contact
+
 const Item = mongoose.model('Item', artSchema);
 
 module.exports = Item;
